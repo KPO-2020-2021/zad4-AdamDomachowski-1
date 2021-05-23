df2d46ea1932da9317481a02ccfe8a4ef74302fb
Adam <adam@adam-ASUS-TUF-Gaming-A15-FA506II-FA506II>
2021-05-23 21:30:25 +0200

    commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/adam/programowanie/zad4-AdamDomachowski-1
+# Build directory: /home/adam/programowanie/zad4-AdamDomachowski-1/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("tests")
